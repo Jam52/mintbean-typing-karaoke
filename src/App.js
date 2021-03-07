a76a0601374dfd400faa6d1636242c7528371536
Mona Zheng <mona.a.zheng@gmail.com>
2021-03-07 17:20:18 -0500

    added url routes to game page and landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import GamePage from './Game Components/GamePage';
 import LandingPage from './Game Components/LandingPage';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <h1>routes for game page and landing page</h1>
-      </Switch>
-    </Router>
+    <div>
+      <h1>Memory Maddening</h1>
+      <Router>
+        <Switch>
+          <Route exact path='/about'>
+            <LandingPage />
+          </Route>
+          <Route path='/'>
+            <GamePage/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+    
   );
 }
 
