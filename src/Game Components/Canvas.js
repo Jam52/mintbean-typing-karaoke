7c0478d93ddc3a9adfc499efc4b20c9e518ce052
Jam52 <jamie.sajdak@gmail.com>
2021-03-08 11:58:08 -0500

    Add project breakdown comments and basic state

--- src/Game Components/Canvas.js
+++ src/Game Components/Canvas.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Canvas = () => {
+const Canvas = (props) => {
+  const {
+    revealLength,
+    revealIntervalLength,
+    numWordsRevealed,
+  } = props.gameSettings;
 
-  return(
+  //toDo: funtion to randomly grab new word(s) and add to correctWords Array
+
+  return (
+    //toDo: checks if timer is inside reveal window and calls functions to get words and reveal them
+
+    //toDo: check is timer is at 0 and isGameFinished to true
     <div>
       <h1>Canvas</h1>
     </div>
-  )
-}
-export default Canvas
+  );
+};
+export default Canvas;
--- src/Game Components/GamePage.js
+++ src/Game Components/GamePage.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Canvas from './Canvas';
+import StartGame from './StartGame';
 
 const GamePage = () => {
+  //words fetched from api when game is started (so there are new words each time)
+  const [wordsData, setWordsData] = useState(null);
+  const [gameSettings, setGameSettings] = useState({
+    revealLength: 500,
+    revealIntervalLength: 3000,
+    numWordsRevealed: 1,
+    isGameRunning: false,
+  });
+  const [correctWordsArray, setCorrectWordsArray] = useState([]);
+  const [score, setScore] = useState(0);
+  const [userInput, setUserInput] = useState('');
+  const [isGameFinished, setIsGameFinished] = useState(false);
+  //toDo: timer state, could use a hook
 
-  return(
+  //toDo: keyboard listener function listends to keyboard and updates userInput, checks if word matches word in correctWordsArray and update score
+
+  //toDo: gameStartHandler, sets wordsData, updates Game Settings(can be expanded), Starts timer starts listener for keybord input
+  const gameStartHandler = () => {};
+
+  return (
     <div>
       <h1>Game Page</h1>
+      {gameSettings.isGameRunning ? (
+        <Canvas
+          wordsData={wordsData}
+          gameSettings={gameSettings}
+          setCorrectWordsArray={setCorrectWordsArray}
+          setIsGameFinished={setIsGameFinished}
+        />
+      ) : isGameFinished ? (
+        <Score score={score} />
+      ) : (
+        <StartGame />
+      )}
     </div>
-  )
-}
-export default GamePage
+  );
+};
+export default GamePage;
